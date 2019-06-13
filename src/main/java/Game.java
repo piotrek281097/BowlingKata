ce4ddd80f24b983502430a82e244c43f39954c04
Piotr Plecinski <209432@edu.p.lodz.pl>
2019-06-13 11:49:13 +0200

    refactor after 3 tests

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,6 +3,12 @@ public class Game {
     private int roll = 0;
     private int[] rolls = new int[21];
 
+    public void roll(int... rolls) {
+        for (int pinsDown : rolls) {
+            roll(pinsDown);
+        }
+    }
+
     public void roll(int pinsDown) {
         rolls[roll++] = pinsDown;
     }
@@ -11,7 +17,7 @@ public class Game {
         int score = 0;
         int cursor = 0;
         for(int frame = 0; frame < 10; frame++) {
-            if(rolls[cursor] + rolls[cursor + 1] == 10) {
+            if(isSpare(cursor)) {
                 score += 10 + rolls[cursor + 2];
                 cursor += 2;
             } else {
@@ -21,5 +27,11 @@ public class Game {
         }
 
         return score;
+    }
+
+
+
+    private boolean isSpare(int cursor) {
+        return rolls[cursor] + rolls[cursor + 1] == 10;
     }
 }
