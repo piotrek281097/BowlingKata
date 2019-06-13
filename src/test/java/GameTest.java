8f1de3aad2c447f453a6ea67719cd03c05ce4171
Piotr Plecinski <209432@edu.p.lodz.pl>
2019-06-13 11:03:29 +0200

    first test canMakeGame added

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,11 @@
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class GameTest {
+
+    @Test
+    public void canMakeGame() {
+        new Game();
+    }
+}
